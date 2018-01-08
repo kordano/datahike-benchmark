70768fac017fcf2b57596dc27caa200f6f2141cc
Konrad Kühne <konnykuehne@gmail.com>
2018-01-08 16:47:14 +0100

    initialize project

---
+++ test/datahike_benchmark/core_test.clj
@@ -0,0 +1,7 @@
+(ns datahike-benchmark.core-test
+  (:require [clojure.test :refer :all]
+            [datahike-benchmark.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
