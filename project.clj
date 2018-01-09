c19b5af51a9a893a94acdf0476d617f5d6fa086b
Konrad Kühne <konnykuehne@gmail.com>
2018-01-09 15:01:42 +0100

    use latest clojure version

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [criterium "0.4.4"]
+                 [com.datomic/datomic-free "0.9.5394"]
+                 [io.replikativ/datahike "0.1.0-SNAPSHOT"]])
